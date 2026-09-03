ad8be4c22cfeec57b0b195a84f1ee595fecbfec6
hammon737 <hammon737@morrisville.edu>
2026-09-03 17:19:54 -0400

    ...

---
+++ Class3_demo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Class3_demo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Class3_demo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Class3_demo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
