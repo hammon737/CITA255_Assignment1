1188b3531f7cab7cf674677934a2c7fa0307ebd7
hammon737 <hammon737@morrisville.edu>
2026-09-08 17:34:25 -0400

    fixed number error

--- Class3_demo/MainPage.xaml.cs
+++ Class3_demo/MainPage.xaml.cs
@@ -12,7 +12,7 @@
         private void Numbers(object sender, EventArgs e)
         {
             float fahrenheitNumber = float.Parse(fahrenheit.Text);
-            float celsius = (fahrenheitNumber - 32) * 5 / 9;
+            float celsius = (fahrenheitNumber - 32) * 5.0f / 9.0f;
             celsiusLabel.Text = $"This temperature in Celsius is {celsius:N1} degrees.";
         }
     }
